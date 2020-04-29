4678eeee888b5b5ff8b426d6728247cdbc548f88
Vishal Nath Chauhan <63584407+vishal-nath-chauhan@users.noreply.github.com>
2020-04-29 17:40:17 +0530

    correct commit

--- sentiment_analysis/analysis.py
+++ sentiment_analysis/analysis.py
@@ -10,48 +10,51 @@ from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 import re
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.externals import joblib
+import joblib
 from sklearn.naive_bayes import GaussianNB,MultinomialNB
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import StackingClassifier
 
 class analyzer():
-	def __init__(self):
-		self.stop=stopwords.words("english")
-		self.punctuation=string.punctuation
-		self.stem=PorterStemmer()
-		self.TfidfVectorizer=TfidfVectorizer
-		self.word_tokenize=word_tokenize
-		self.LogisticRegression=LogisticRegression
-		self.MultinomialNB=MultinomialNB
-		self.clean="@\S+|https?:\S+|http?:\S|"
-		self.full_words={"can't":"can not","aren't":"are not","isn't":"is not","that's":"that is","i'm":"i am","it's":"it is"}
-	def preprocess(self,text):
-	    corp=re.sub(self..clean,"",str(text).lower())
-	    words=word_tokenize(corp)
-	    out=""
-	    for i in words:
-	        if i not in stop and punctuation:
-	            out+=" "+stem.stem(i)
-	   
-	    V=joblib.load('saved_V')
-	    transformed=V.transform(out)
-	    return transformed
+    def __init__(self):
+        self.stop=stopwords.words("english")
+        self.re=re
+        self.joblib=joblib
+        self.punctuation=string.punctuation
+        self.stem=PorterStemmer()
+        self.TfidfVectorizer=TfidfVectorizer
+        self.word_tokenize=word_tokenize
+        self.LogisticRegression=LogisticRegression
+        self.MultinomialNB=MultinomialNB
+        self.clean="@\S+|https?:\S+|http?:\S|"
+        self.full_words={"can't":"can not","aren't":"are not","isn't":"is not","that's":"that is","i'm":"i am","it's":"it is"}
+    def preprocess(self,text):
+        corp=self.re.sub(self.clean,"",str(text).lower())
+        words=self.word_tokenize(corp)
+        out=""
+        for i in words:
+            if i not in self.stop and self.punctuation:
+                out+=" "+self.stem.stem(i)
+        
+        V=joblib.load('saved_V')
+        transformed=V.transform([out])
+        return transformed
         
 
-	
 
-	def output(self,text):
-		self.nb=self.MultinomialNB()
-		self.lr=self.LogisticRegression(n_jobs=-1,random_state=32)
-		stacking=joblib.load('stacked_model')
-	    out=self.preprocess(text)
-	    result=nb.predict(out)+lr.predict(out)
-	    if result == [0]:
-	    	return 0
-	        # print(f"{text} ==> Negative sentiment")
 
-	    else:
-	    	return 1
-	        # print(f"{text} ==>Positive sentiment")
+    def output(self,text):
+        self.nb=self.MultinomialNB()
+        self.lr=self.LogisticRegression(n_jobs=-1,random_state=32)
+        stacking=self.joblib.load('stacked_model')
+        out=self.preprocess(text)
+        result=stacking.predict(out)
+       
+        if result == 0:
+            return 0
+            # print(f"{text} ==> Negative sentiment")
+
+        if result == 4:
+            return 1
+            # print(f"{text} ==>Positive sentiment")
--- sentiment_analysis/app.py
+++ sentiment_analysis/app.py
@@ -1,24 +1,27 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
-from sklearn.externals import joblib
+import joblib
 from analysis import analyzer
 
 analyzer=analyzer()
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates')
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/',methods=['POST'])
 def predict():
 
-    text = request.form.values()
+    text = request.form['Email id']
+    # print(jsonify(text))
     output = analyzer.output(text)
-    if output==0:
+    if output == 0:
         return render_template('negative.html')
+        # return  jsonify(text)
     if output ==1:
-        render_template('positive.html')
+        return render_template('positive.html')
+        # return  jsonify(text)
 
 
 
